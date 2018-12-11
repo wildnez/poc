062f8019f8e9acbd4c36908e841840cc3dce6a3c
Rahul Gupta <rahul@hazelcast.com>
2018-12-11 13:28:40 +0530

    Update POCUserCodeDepDataModel.java

--- src/main/java/poc/hazelcast/POCUserCodeDepDataModel.java
+++ src/main/java/poc/hazelcast/POCUserCodeDepDataModel.java
@@ -10,8 +10,8 @@ public class POCUserCodeDepDataModel implements DataSerializable {
 
     private long field1;
     private long field2;
-    private long field3;
-    private long field4;
+   // private long field3;
+   // private long field4;
 
     public POCUserCodeDepDataModel() {
     }
@@ -21,37 +21,33 @@ public class POCUserCodeDepDataModel implements DataSerializable {
         this.field2 = field2;
     }
 
-    public POCUserCodeDepDataModel(long field1, long field2, long field3) {
-        this.field1 = field1;
-        this.field2 = field2;
-        this.field3 = field3;
-    }
-
+    /*
     public POCUserCodeDepDataModel(long field1, long field2, long field3, long field4) {
         this.field1 = field1;
         this.field2 = field2;
         this.field3 = field3;
         this.field4 = field4;
     }
+    */
 
     @Override
     public void writeData(ObjectDataOutput objectDataOutput) throws IOException {
         objectDataOutput.writeLong(field1);
         objectDataOutput.writeLong(field2);
-        objectDataOutput.writeLong(field3);
-        objectDataOutput.writeLong(field4);
+        //objectDataOutput.writeLong(field3);
+        //objectDataOutput.writeLong(field4);
     }
 
     @Override
     public void readData(ObjectDataInput objectDataInput) throws IOException {
         this.field1 = objectDataInput.readLong();
         this.field2 = objectDataInput.readLong();
-        this.field3 = objectDataInput.readLong();
-        this.field4 = objectDataInput.readLong();
+        //this.field3 = objectDataInput.readLong();
+        //this.field4 = objectDataInput.readLong();
     }
 
     @Override
     public String toString() {
-        return "field 1: " + field1 + " field 2: " + field2 + " field 3: " + field3 + " field 4: " + field4;
+        return "field 1: " + field1 + " field 2: " + field2;// + " field 3: " + field3 + " field 4: " + field4;
     }
 }
